c559a8b039eaf0a2bb288a8abd84ab4c584f57df
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2019-10-22 17:35:00 +0400

    Add files via upload

---
+++ 22.10.2019 Js Loop,Array/js/script.js
@@ -0,0 +1,87 @@
+// let i = 5;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//     if (i == 5)
+//         break;
+
+// }
+
+// let i = 0;
+// let result = 0;
+// i = 100;
+
+// while (i < 10) {
+//     i++;
+//     result += i;
+//     console.log(result)
+// }
+
+// let x = 1;
+
+// do {
+//     x++;
+//     console.log(x);
+
+// } while (x <= 10)
+
+// var student = { fname: "John", lname: "Doe", age: 25 };
+// console.log(student["fname"])
+// var students = ["Memmed", "Miri", "Elgiz"];
+
+// for (x in student) {
+//     console.log(student[x])
+// }
+
+// console.log(students[students.length - 1])
+
+// for (let i = 0; i < students.length; i++) {
+//     console.log(students[i]);
+
+// }
+
+// for (let s of students) {
+//     console.log(s);
+// }
+
+
+
+
+// var std=new Array()
+
+// var stu = ["Memmed", "Miri", "Elgiz", "Ismayil"];
+
+// console.log(stu.join('a'))
+
+// let name = stu.pop();
+
+// stu.push("Ibrahim");
+// stu.shift();
+// stu.shift();
+// stu[0] = "Huseyn"
+// stu.splice(1, 0, "Ibrahim", 10, "Sevda")
+
+// console.log(stu)
+
+
+//TASK-1
+
+// let number = 100; //100*100*100
+// let power = 3;
+
+// let result = 1;
+
+// for (let i = 0; i < power; i++) {
+//     result *= number; //1*100*100*100
+// }
+
+// console.log(result)
+
+//TASK-2
+var arr = ["Elgiz", "Elman", "Sevda"];
+var symbol = "-";
+
+console.log(arr.join('-'))
+
+
+// Elgiz-Elman-Sevda
